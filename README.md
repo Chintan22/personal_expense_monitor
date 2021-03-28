299288ad6269a32f64cc224713d88a230ba65070
Chintan22 <Dankharachintan@gmail.com>
2021-03-28 19:39:04 -0400

    Draft 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # personal_expense_monitor
 
 This is a basic personal expense monitor application based on flutter/dart.
+You can add multiple entries with title, cost and date to monitor your expenses.
+Based on added entries, an automatically generated chart will show you cost for every day expenses.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './entryList.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,18 +33,64 @@ class MyHomePage extends StatelessWidget {
             Card(
               child: Text('Top for chart'),
             ),
+            Card(
+              elevation: 7,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Add Title'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Add Amount'),
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text('Add Entry !'),
+                      textColor: Colors.blue.shade600,
+                    )
+                  ],
+                ),
+              ),
+            ),
             Column(
               children: entries.map((et) {
                 return Card(
                   child: Row(
                     children: <Widget>[
                       Container(
-                        child: Text(et.cost.toString()),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.blue,
+                              width: 3,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text('\$${et.cost}',
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700)),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(et.entryName),
-                          Text(et.date.toString())
+                          Text(
+                            et.entryName,
+                            style: TextStyle(
+                                color: Colors.cyan,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMd().add_jm().format(et.date),
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.indigo),
+                          )
                         ],
                       )
                     ],
